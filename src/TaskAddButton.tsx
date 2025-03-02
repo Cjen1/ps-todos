7ffa69d9d009b0bebc2022943fdc66d9013f4a7d
cjen1 <cjj39@cam.ac.uk>
2025-03-02 16:48:54 +0000

    valtio

--- src/TaskAddButton.tsx
+++ src/TaskAddButton.tsx
@@ -1,9 +1,15 @@
 import type { FC } from "react";
 import styles from "./TaskAddButton.module.css";
+import type {TaskStatus} from "./types";
+import { addTask } from "./taskStore";
 
-export const TaskAddButton: FC = () => {
+interface Props {
+  status; TaskStatus;
+}
+
+export const TaskAddButton: FC<Props> = ({ status }) => {
   return (
-    <button type="button" className={styles.button}>
+    <button type="button" className={styles.button} onClick={() => addTask(status)}>
       + Add
     </button>
   );
