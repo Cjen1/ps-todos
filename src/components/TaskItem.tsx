50399a1ae4a5186b5972f7a093bdf22daa2a73f7
cjen1 <cjj39@cam.ac.uk>
2025-03-09 15:47:44 +0000

    Add long press settings popup

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,6 +1,6 @@
 import { useDraggable } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
-import { type ChangeEvent, type FC, useCallback } from "react";
+import { type ChangeEvent, type FC, useCallback, useState } from "react";
 import styles from "./TaskItem.module.css";
 import {
   type YTask,
@@ -12,7 +12,24 @@ import { DragHandleSVG, DeleteHandleSVG } from "../assets/SvgHandles";
 import TextareaAutosize from "react-textarea-autosize";
 import useLongPress from "./useLongPress";
 
+
 export const TaskItem: FC<{ taskstore: TaskStore, tid: string }> = ({ taskstore, tid }) => {
+  useY(taskstore.ytasks.get(tid) as YTask);
+
+  // Settings popup
+
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+  const settingsComponent = isSettingsOpen && (
+    <div className={styles["settings-overlay"]}>
+      <div className={styles["settings"]}>
+        <h2> Settings for {tid} </h2>
+        <button onClick={() => setIsSettingsOpen(false)}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+
   // render dnd drag
   const { attributes, listeners, setNodeRef, transform, isDragging } =
     useDraggable({ id: tid });
@@ -20,13 +37,8 @@ export const TaskItem: FC<{ taskstore: TaskStore, tid: string }> = ({ taskstore,
     transform: CSS.Translate.toString(transform),
   };
 
-  useY(taskstore.ytasks.get(tid) as YTask);
-
   const onInput = useCallback((event: ChangeEvent<HTMLTextAreaElement>) => {
     taskstore.ytasks.get(tid)?.set(YTaskAccessors.DESCRIPTION, event.target.value);
-  }, []);
-  const onComplete = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-    taskstore.ytasks.get(tid)?.set(YTaskAccessors.COMPLETE, event.target.checked);
   }, []);
   const handleDelete = useCallback((_: any) => {
     taskstore.ytasks.delete(tid);
@@ -51,12 +63,13 @@ export const TaskItem: FC<{ taskstore: TaskStore, tid: string }> = ({ taskstore,
     </button>
   );
 
-  const checkboxCheck = () => {
-    taskstore.ytasks.get(tid)?.set(YTaskAccessors.COMPLETE, !taskstore.getTaskField(tid, YTaskAccessors.COMPLETE));
-  };
-
-  const longPressElement = useLongPress(() => console.log("LongPress"), checkboxCheck);
-
+  const checkbox_callbacks = useLongPress(
+    // Long press
+    () => { console.log("Opening settings") ;setIsSettingsOpen(true);},
+    // On click
+    () => taskstore.ytasks.get(tid)?.set(YTaskAccessors.COMPLETE, !taskstore.getTaskField(tid, YTaskAccessors.COMPLETE)),
+  );
+    
   const checkbox_style = (taskstore.getTaskField(tid, YTaskAccessors.COMPLETE) as boolean) ? styles["complete-checkbox"] : styles["incomplete-checkbox"];
 
   return (
@@ -65,17 +78,18 @@ export const TaskItem: FC<{ taskstore: TaskStore, tid: string }> = ({ taskstore,
       ref={setNodeRef}
       style={style}
     >
+      {settingsComponent}
       <div className={styles['wrapper-checkbox']}>
         <button
           type="button"
           className={`${styles["base-checkbox"]} ${checkbox_style}`}
-          {...longPressElement}
+          {...checkbox_callbacks}
         />
       </div>
       <div className={styles.inputwrapper}>
         <TextareaAutosize
           className={styles.input}
-          value={taskstore.getTaskField(tid, YTaskAccessors.DESCRIPTION) as string}
+          value={false ? tid : taskstore.getTaskField(tid, YTaskAccessors.DESCRIPTION) as string}
           onChange={onInput}
         />
       </div>
