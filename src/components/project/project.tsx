499b2f8141ac30371f913b12486903d7ce145aae
cjen1 <cjj39@cam.ac.uk>
2025-04-30 06:28:36 +0100

    Add dnd

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -1,11 +1,15 @@
-import { FC } from "react";
+import { FC, Fragment, useCallback } from "react";
 import { AutomergeUrl } from "@automerge/automerge-repo";
 import { Label } from "@/components/ui/label";
 import { useDocument, useRepo } from "@automerge/automerge-repo-react-hooks";
-import { add_new_task, Project as ProjectData } from "./store";
+import { add_new_task, move_task, Project as ProjectData } from "./store";
 import { object_map } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Task } from "@/components/task/task";
+import { Separator } from "@/components/ui/separator";
+
+import { DroppableMarker } from "@/components/dnd/DroppableMarker";
+import { DndContext, type DragEndEvent } from "@dnd-kit/core";
 
 export const Project: FC<{ project_url: AutomergeUrl, petname: string }> = ({ project_url, petname }) => {
     const [project, changeDoc] = useDocument<ProjectData>(project_url);
@@ -14,26 +18,41 @@ export const Project: FC<{ project_url: AutomergeUrl, petname: string }> = ({ pr
         return <Label>Error: url invalid {project_url}</Label>
     }
 
+    const handleDragEnd = (event: DragEndEvent) => {
+      if (!event.over) {
+        return;
+      }
+      const data = event.over.data.current;
+      move_task(changeDoc, event.active.id, data?.prev_id, data?.next_id);
+    };
+
     return (
-        <div className="bg-card flex flex-col gap-2 p-2">
-            <Label className="justify-center">{petname}</Label>
-            {object_map(project.tasks, (task_url, { order }) => {
-                return { order: order, task_url: task_url };
-            }).sort((a, b) => a.order - b.order).map(({ task_url }) => {
-                return (
-                    <Task
-                        key={task_url}
-                        project_url={project_url as AutomergeUrl}
-                        task_url={task_url as AutomergeUrl} />
-                );
-            })}
-            <Button
-                type="button"
-                variant="outline"
-                className="w-full"
-                onClick={() => add_new_task(changeDoc)}>
-                Add Task
-            </Button>
-        </div>
+        <DndContext onDragEnd={handleDragEnd}>
+            <div className="bg-card flex flex-col gap-2 p-2">
+                <Label className="justify-center">{petname}</Label>
+                {object_map(project.tasks, (task_url, { order }) => {
+                    return { order: order, task_url: task_url };
+                }).sort((a, b) => a.order - b.order).map(({ task_url }) => {
+                    return (
+                      <Fragment key={task_url}>
+                        <DroppableMarker/>
+                        <Task
+                            key={task_url}
+                            project_url={project_url as AutomergeUrl}
+                            task_url={task_url as AutomergeUrl} />
+                      </Fragment>
+                    );
+                })}
+                <DroppableMarker/>
+                <Separator/>
+                <Button
+                    type="button"
+                    variant="outline"
+                    className="w-full"
+                    onClick={() => add_new_task(changeDoc)}>
+                    Add Task
+                </Button>
+            </div>
+        </DndContext>
     );
 }
--- src/components/project/store.tsx
+++ src/components/project/store.tsx
@@ -34,3 +34,24 @@ export function delete_task(changeDoc: ChangeDoc, task_url: AutomergeUrl) : void
         delete doc.tasks[task_url];
     });
 }
+
+function calculate_next_order(prev_order: number | null, next_order: number | null) {
+  let low = 0;
+  let high = 1;
+  if (prev_order && next_order) {
+      low = Math.min(prev_order, next_order);
+      high = Math.max(prev_order, next_order);
+  } else if (prev_order || next_order) {
+      const v = prev_order ?? next_order;
+      low = Math.min(v, 0);
+      high = Math.max(v, 1);
+  } 
+  return low + Math.random() * (high - low);
+}
+
+export function move_task(changeDoc: ChangeDoc, task_url: AutomergeUrl, prev_url: AutomergeUrl | null, next_url: AutomergeUrl | null) {
+    changeDoc((doc) => {
+      doc.tasks[task_url].order = 
+          calculate_next_order(prev_url ?? doc.tasks[prev_url]?.order, next_url ?? doc.tasks[next_url]?.order);
+    });
+}
