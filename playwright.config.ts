d92aad96662ffe42c53402b635ded73bd1949e9f
cjen1 <cjjensen01@aol.com>
2026-07-05 23:33:29 +0100

    Print Playwright steps in test output

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,9 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests/e2e",
   fullyParallel: true,
+  reporter: process.env.CI
+    ? [["github"], ["list", { printSteps: true }]]
+    : [["list", { printSteps: true }]],
   use: {
     baseURL: "http://127.0.0.1:5173",
     trace: "on-first-retry",
